52b11b0f8c1036609354a7b576ffec4accb66495
Snehaa244 <127610208+Snehaa244@users.noreply.github.com>
2024-04-12 19:37:08 +0530

    Pokedex.jsx

--- src/Components/Pokedex/Pokedex.jsx
+++ src/Components/Pokedex/Pokedex.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import PokemonList from "../PokemonList/PokemonList";
 import Search from "../Search/Search";
 
-//css import
+// CSS import
 import './Pokedex.css';
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
 
-function Pokedex(){
-return(
-<div className="pokedex-wrapper">
-<h1 id="pokedex-heading">Pokedex</h1>
-<Search/>
-<PokemonList/>
-</div>
-)
+function Pokedex() {
+
+    const [searchTerm, setSearchterm] = useState('');
+
+    return (
+        <div className="pokedex-wrapper">
+            <Search updateSearchTerm={setSearchterm} />
+            { (!searchTerm) ? <PokemonList /> : <PokemonDetails key={searchTerm} pokemonName={searchTerm} />}
+        </div>
+    )
 }
+
 export default Pokedex;
