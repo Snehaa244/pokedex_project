ed92430a21f6e70f52744b74982a0350f7c3b8d6
Snehaa244 <127610208+Snehaa244@users.noreply.github.com>
2024-04-12 19:39:06 +0530

    Pokemon.jsx

--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,9 +1,17 @@
-function Pokemon({ name , image }){
-return(
-    <div>
-        <div>{name}</div>
-        <div><img src={image}/></div>
-    </div>
-)
+import { Link } from 'react-router-dom';
+import './Pokemon.css'
+
+function Pokemon({ name, image, id }) {
+    return (
+        <div className='pokemon'>
+            <Link to={`/pokemon/${id}`}>
+                <div className='pokemon-name'>{name}</div>
+                <div>
+                    <img className='pokemon-image' src={image} />
+                </div>
+            </Link>
+        </div>
+    )
 }
+
 export default Pokemon;
