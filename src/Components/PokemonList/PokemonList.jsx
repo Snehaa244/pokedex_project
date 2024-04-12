23204429e2d8aa8b7ca811db1cd08ea86f3c3bb2
Snehaa244 <127610208+Snehaa244@users.noreply.github.com>
2024-04-12 19:47:09 +0530

    PokemonList.jsx

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -1,56 +1,32 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-import './PokemonList.css';
+import './PokemonList.css'
 import Pokemon from "../Pokemon/Pokemon";
-function PokemonList(){
-    const [pokemonList,setPokemonList] = useState([]);
-    const [isLoading,setIsLoading] = useState(true);
-
-//useEffect acept a call back and a dependency array,call back k ander jo bhi logic likhnge vo execute hoga,if dependency array nhi pass krnge to pura logic bar bar rerender hoga
-    //jab dependancy array banyge to jab phle bar component execute ho jabhi logic render hoga uske bad rerender nhi hoga
-    //agar kisi variable ya kisi state variable pe k chnage ko track krna chate ho jab component rerender hoga aur useeffect call hoga
-    //eslint mtlb yellow line ki problem can be solved bu=y googling it
-   
-    async function downloadpokemons()
-    {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');//this download 20 list of pokemon
-
-        const pokemonResults = response.data.results;//we get array of pokemon from the result
-        console.log(response.data);
-
-            //iterating over the array of pokemons, and using their url, to create array of promises
-            //that will load those 20 pokemon
-        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
-          //passing that promise array to axios.all
-        const pokemonData = await axios.all(pokemonResultPromise);//array of 20 pokemon detailed data
-        console.log(pokemonData);
-        //now iterate on the data of each promise and extract id,name,image,types
-        const pokeListResult = (pokemonData.map((pokeData) => {
-             const pokemon =pokeData.data;
-             return{
-                  id:pokemon.id,
-                  name: pokemon.name,
-                  image:( pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default :pokemon.sprites.front_shinny,
-                  types:pokemon.types}
-        }));
-        console.log(pokeListResult);
-        setPokemonList(pokeListResult);
-        setIsLoading(false);
-    }
-    useEffect (() => {
-        downloadpokemons();
-    },[]);
-return(
-    <div className="pokemon-list-wrapper">
-        <div>Pokemon List</div>
-       <div className="pokemon-wrapper">
-         { (isLoading) ? 'Loading....':
-        pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)
-        }
+import usePokemonList from "../../hooks/usePokemonList";
+import { useEffect } from 'react';
+function PokemonList() {
+    const [ pokemonListState, setPokemonListState] = usePokemonList(false);
+    useEffect(() => {
+        console.log("render")
+    })
+    return (
+        <div className="pokemon-list-wrapper">
+            <div className="pokemon-wrapper">
+                {(pokemonListState.isLoading) ? 'Loading....' : 
+                    pokemonListState.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
+                }
+            </div>
+            <div className="controls">
+                <button disabled={pokemonListState.prevUrl == null} onClick={() => {
+                    const urlToSet = pokemonListState.prevUrl;
+                    setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet})
+                }}>Prev</button>
+                <button disabled={pokemonListState.nextUrl == null} onClick={() => {
+                    console.log(pokemonListState)
+                    const urlToSet = pokemonListState.nextUrl;
+                    setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet})
+                }}>Next</button>
+            </div>
         </div>
-        </div>
-    
-
-)
+    )
 }
+
 export default PokemonList;
